ad8cdf0ed4f2b6f3e2586dc5c6dd0f922a556972
Aron Woost <aron@springworks.de>
2014-02-12 22:02:56 +0100

    Add expand selection to word

--- ExpandRegion.py
+++ ExpandRegion.py
@@ -1,6 +1,41 @@
-import sublime_plugin
-from basic_expansions import foo
+import sublime, sublime_plugin, re
 
 class ExpandRegionCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    foo();
+    region = self.view.sel()[0]
+    string = self.view.substr(sublime.Region(0, self.view.size()))
+    start = region.begin()
+    end = region.end()
+
+    if self.expand_to_word(string, start, end) is None:
+      print "none"
+  
+  def expand_to_word(self, string, startIndex, endIndex):
+    wordRe = re.compile("^[a-zA-Z0-9_]*$");
+
+    search = True;
+    searchIndex = startIndex;
+    while search:
+      char = string[searchIndex:searchIndex+1]
+      if wordRe.match(char) is None:
+        newStartIndex = searchIndex + 1
+        search = False
+      else:
+        searchIndex -= 1
+
+    search = True;
+    searchIndex = endIndex;
+    while search:
+      char = string[searchIndex:searchIndex+1]
+      if wordRe.match(char) is None:
+        newEndIndex = searchIndex
+        search = False
+      else:
+        searchIndex += 1
+
+
+    if startIndex == newStartIndex and endIndex == newEndIndex:
+      return None
+    else:
+      self.view.sel().add(sublime.Region(newStartIndex, newEndIndex))
+      return True
