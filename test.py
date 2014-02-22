735f04cff51b0cb9e61431cf05983922793fd9ee
Aron Woost <aron@springworks.de>
2014-02-22 14:08:30 +0100

    Tests: update structure for better readability and upcoming additions

--- test.py
+++ test.py
@@ -7,26 +7,41 @@ class WordTest(unittest.TestCase):
       self.string1 = myfile.read()
 
   def test_word_with_whitespaces_around (self):
-    self.assertEqual(expand_region_handler.expand_to_word(" hello ", 3, 3), {"start": 1, "end": 6, "string": "hello", "type": "word"})
+    result = expand_region_handler.expand_to_word(" hello ", 3, 3);
+    self.assertEqual(result["start"], 1)
+    self.assertEqual(result["end"], 6)
+    self.assertEqual(result["string"], "hello")
 
   def test_dont_find_word (self):
-    self.assertEqual(expand_region_handler.expand_to_word(self.string1, 1, 10), None)
+    result = expand_region_handler.expand_to_word(self.string1, 1, 10);
+    self.assertEqual(result, None)
 
   def test_dont_find_word2 (self):
-    self.assertEqual(expand_region_handler.expand_to_word(" ee ee ", 2, 5), None)
+    result = expand_region_handler.expand_to_word(" ee ee", 2, 5);
+    self.assertEqual(result, None)
 
   def test_string_is_only_word (self):
-    self.assertEqual(expand_region_handler.expand_to_word("bar", 1, 1), {"start": 0, "end": 3, "string": "bar", "type": "word"})
+    result = expand_region_handler.expand_to_word("bar", 1, 1);
+    self.assertEqual(result["start"], 0)
+    self.assertEqual(result["end"], 3)
+    self.assertEqual(result["string"], "bar")
 
   def test_dont_find_word3 (self):
-    self.assertEqual(expand_region_handler.expand_to_word("foo.bar", 5, 5), {"start": 4, "end": 7, "string": "bar", "type": "word"})
+    result = expand_region_handler.expand_to_word("foo.bar", 5, 5);
+    self.assertEqual(result["start"], 4)
+    self.assertEqual(result["end"], 7)
+    self.assertEqual(result["string"], "bar")
 
   def test_dont_find_word3_and_dont_hang (self):
-    self.assertEqual(expand_region_handler.expand_to_word("aaa", 0, 3), None)
+    result = expand_region_handler.expand_to_word("aaa", 0, 3);
+    self.assertEqual(result, None)
 
 class WordWithDotsTest(unittest.TestCase):
   def test (self):
-    self.assertEqual(expand_region_handler.expand_to_word_with_dots("foo.bar", 6, 7), {"start": 0, "end": 7, "string": "foo.bar", "type": "word_with_dots"})
+    result = expand_region_handler.expand_to_word_with_dots("foo.bar", 6, 7);
+    self.assertEqual(result["start"], 0)
+    self.assertEqual(result["end"], 7)
+    self.assertEqual(result["string"], "foo.bar")
 
 class QuoteTest(unittest.TestCase):
   def setUp(self):
@@ -36,16 +51,28 @@ class QuoteTest(unittest.TestCase):
       self.string2 = myfile.read()
 
   def test_double_quotes_inner (self):
-    self.assertEqual(expand_region_handler.expand_to_quotes(self.string1, 6, 12), {"start": 1, "end": 12, "string": "test string", "type": "quotes"})
+    result = expand_region_handler.expand_to_quotes(self.string1, 6, 12);
+    self.assertEqual(result["start"], 1)
+    self.assertEqual(result["end"], 12)
+    self.assertEqual(result["string"], "test string")
 
   def test_double_quotes_outer (self):
-    self.assertEqual(expand_region_handler.expand_to_quotes(self.string1, 1, 12), {"start": 0, "end": 13, "string": "\"test string\"", "type": "quotes"})
+    result = expand_region_handler.expand_to_quotes(self.string1, 1, 12);
+    self.assertEqual(result["start"], 0)
+    self.assertEqual(result["end"], 13)
+    self.assertEqual(result["string"], "\"test string\"")
 
   def test_single_quotes_inner (self):
-    self.assertEqual(expand_region_handler.expand_to_quotes(self.string2, 6, 12), {"start": 1, "end": 12, "string": "test string", "type": "quotes"})
+    result = expand_region_handler.expand_to_quotes(self.string2, 6, 12);
+    self.assertEqual(result["start"], 1)
+    self.assertEqual(result["end"], 12)
+    self.assertEqual(result["string"], "test string")
 
   def test_single_quotes_outer (self):
-    self.assertEqual(expand_region_handler.expand_to_quotes(self.string2, 1, 12), {"start": 0, "end": 13, "string": "'test string'", "type": "quotes"})
+    result = expand_region_handler.expand_to_quotes(self.string2, 1, 12);
+    self.assertEqual(result["start"], 0)
+    self.assertEqual(result["end"], 13)
+    self.assertEqual(result["string"], "'test string'")
 
 class SymbolTest(unittest.TestCase):
   def setUp(self):
@@ -55,21 +82,30 @@ class SymbolTest(unittest.TestCase):
       self.string2 = myfile.read()
 
   def test_symbol_inner (self):
-    self.assertEqual(expand_region_handler.expand_to_symbols(self.string1, 7, 10), {"start": 1, "end": 10, "string": "foo - bar", "type": "symbol"})
+    result = expand_region_handler.expand_to_symbols(self.string1, 7, 10);
+    self.assertEqual(result["start"], 1)
+    self.assertEqual(result["end"], 10)
+    self.assertEqual(result["string"], "foo - bar")
 
   def test_symbol_outer (self):
-    self.assertEqual(expand_region_handler.expand_to_symbols(self.string1, 1, 10), {"start": 0, "end": 11, "string": "(foo - bar)", "type": "symbol"})
+    result = expand_region_handler.expand_to_symbols(self.string1, 1, 10);
+    self.assertEqual(result["start"], 0)
+    self.assertEqual(result["end"], 11)
+    self.assertEqual(result["string"], "(foo - bar)")
 
   def test_look_back_dont_hang (self):
-    testString = "   "
-    self.assertEqual(expand_region_handler.expand(testString, 1, 2), None)
+    result = expand_region_handler.expand_to_symbols("   ", 1, 2);
+    self.assertEqual(result, None)
 
   def test_look_ahead_dont_hang (self):
-    testString = "(   "
-    self.assertEqual(expand_region_handler.expand(testString, 2, 2), None)
+    result = expand_region_handler.expand_to_symbols("(   ", 2, 2);
+    self.assertEqual(result, None)
 
   def test_fix_look_back (self):
-    self.assertEqual(expand_region_handler.expand(self.string2, 32, 32), {"start": 12, "end": 35, "string": "foo.indexOf('bar') > -1", "type": "symbol"})
+    result = expand_region_handler.expand_to_symbols(self.string2, 32, 32);
+    self.assertEqual(result["start"], 12)
+    self.assertEqual(result["end"], 35)
+    self.assertEqual(result["string"], "foo.indexOf('bar') > -1")
 
 class IntegrationTest(unittest.TestCase):
   def setUp(self):
@@ -79,19 +115,39 @@ class IntegrationTest(unittest.TestCase):
       self.string2 = myfile.read()
 
   def test_word (self):
-    self.assertEqual(expand_region_handler.expand(self.string1, 7, 7), {"start": 6, "end": 9, "string": "bar", "type": "word"})
+    result = expand_region_handler.expand(self.string1, 7, 7);
+    self.assertEqual(result["start"], 6)
+    self.assertEqual(result["end"], 9)
+    self.assertEqual(result["string"], "bar")
+    self.assertEqual(result["type"], "word")
 
   def test_quotes_inner (self):
-    self.assertEqual(expand_region_handler.expand(self.string1, 6, 9), {"start": 2, "end": 9, "string": "foo bar", "type": "quotes"})
+    result = expand_region_handler.expand(self.string1, 6, 9);
+    self.assertEqual(result["start"], 2)
+    self.assertEqual(result["end"], 9)
+    self.assertEqual(result["string"], "foo bar")
+    self.assertEqual(result["type"], "quotes")
 
   def test_quotes_outer (self):
-    self.assertEqual(expand_region_handler.expand(self.string1, 2, 9), {"start": 1, "end": 10, "string": "\"foo bar\"", "type": "quotes"})
+    result = expand_region_handler.expand(self.string1, 2, 9);
+    self.assertEqual(result["start"], 1)
+    self.assertEqual(result["end"], 10)
+    self.assertEqual(result["string"], "\"foo bar\"")
+    self.assertEqual(result["type"], "quotes")
 
   def test_symbol_inner (self):
-    self.assertEqual(expand_region_handler.expand(self.string1, 1, 10), {"start": 1, "end": 16, "string": "\"foo bar\" + \"x\"", "type": "symbol"})
+    result = expand_region_handler.expand(self.string1, 1, 10);
+    self.assertEqual(result["start"], 1)
+    self.assertEqual(result["end"], 16)
+    self.assertEqual(result["string"], "\"foo bar\" + \"x\"")
+    self.assertEqual(result["type"], "symbol")
 
   def test_dont_expand_to_dots (self):
-    self.assertEqual(expand_region_handler.expand(self.string2, 2, 5), {"start": 1, "end": 10, "string": " foo.bar ", "type": "quotes"})
+    result = expand_region_handler.expand(self.string2, 2, 5);
+    self.assertEqual(result["start"], 1)
+    self.assertEqual(result["end"], 10)
+    self.assertEqual(result["string"], " foo.bar ")
+    self.assertEqual(result["type"], "quotes")
 
 # def suite():
   # unittest.makeSuite(WordTest, "test")
