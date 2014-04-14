f29e6838eaef79c0c0aadd6934314876b5143af0
Aron Woost <aron@springworks.de>
2014-04-14 22:20:12 +0200

    Add comments

--- expand_region_handler.py
+++ expand_region_handler.py
@@ -128,24 +128,30 @@ def _expand_to_regex_rule(string, startIndex, endIndex, negativeRe, positiveRe, 
     if positiveRe.match(selection) is None:
       return None
 
+  # look back
   searchIndex = startIndex - 1;
   while True:
+    # begin of string is reached
     if searchIndex < 0:
       newStartIndex = searchIndex + 1
       break
     char = string[searchIndex:searchIndex+1]
+    # character found, that does not fit into the search set 
     if negativeRe.match(char) is None:
       newStartIndex = searchIndex + 1
       break
     else:
       searchIndex -= 1
 
+  # look forward
   searchIndex = endIndex;
   while True:
+    # end of string reached
     if searchIndex > len(string) - 1:
       newEndIndex = searchIndex
       break
     char = string[searchIndex:searchIndex+1]
+    # character found, that does not fit into the search set 
     if negativeRe.match(char) is None:
       newEndIndex = searchIndex
       break
@@ -158,6 +164,8 @@ def _expand_to_regex_rule(string, startIndex, endIndex, negativeRe, positiveRe, 
     else:
       return create_return_obj(newStartIndex, newEndIndex, string, type)
   except NameError:
+    # newStartIndex or newEndIndex might not have been defined above, because
+    # the character was not found.
     return None
 
 def expand_to_semantic_unit(string, startIndex, endIndex):
