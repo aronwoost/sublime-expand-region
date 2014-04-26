10673011a8885bf04f6957fd0b34c9095e46d407
Aron Woost <aron@springworks.de>
2014-04-26 22:09:54 +0200

    Improve naming and order of expand_to_word tests

--- test.py
+++ test.py
@@ -59,7 +59,19 @@ class WordTest(unittest.TestCase):
     self.assertEqual(result["end"], 6)
     self.assertEqual(result["string"], "hello")
 
-  def test_dont_find_word (self):
+  def test_find_word_with_dot_before (self):
+    result = expand_to_word.expand_to_word("foo.bar", 5, 5);
+    self.assertEqual(result["start"], 4)
+    self.assertEqual(result["end"], 7)
+    self.assertEqual(result["string"], "bar")
+
+  def test_find_word_when_string_is_only_the_word (self):
+    result = expand_to_word.expand_to_word("bar", 1, 1);
+    self.assertEqual(result["start"], 0)
+    self.assertEqual(result["end"], 3)
+    self.assertEqual(result["string"], "bar")
+
+  def test_dont_find_word1 (self):
     result = expand_to_word.expand_to_word(self.string1, 1, 10);
     self.assertEqual(result, None)
 
@@ -67,23 +79,11 @@ class WordTest(unittest.TestCase):
     result = expand_to_word.expand_to_word(" ee ee", 2, 5);
     self.assertEqual(result, None)
 
-  def test_string_is_only_word (self):
-    result = expand_to_word.expand_to_word("bar", 1, 1);
-    self.assertEqual(result["start"], 0)
-    self.assertEqual(result["end"], 3)
-    self.assertEqual(result["string"], "bar")
-
-  def test_dont_find_word3 (self):
-    result = expand_to_word.expand_to_word("foo.bar", 5, 5);
-    self.assertEqual(result["start"], 4)
-    self.assertEqual(result["end"], 7)
-    self.assertEqual(result["string"], "bar")
-
   def test_dont_find_word3_and_dont_hang (self):
     result = expand_to_word.expand_to_word("aaa", 0, 3);
     self.assertEqual(result, None)
 
-  def test_find_word_dont_include_linebreak (self):
+  def test_dont_expand_to_linebreak (self):
     result = expand_to_word.expand_to_word(self.string2, 0, 0);
     self.assertEqual(result, None)
 
