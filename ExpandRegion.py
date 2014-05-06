a0aff5e1d8b9dc931ca7a8370e1b8ec6bef1d479
Aron Woost <aron@springworks.de>
2014-05-06 23:33:07 +0200

    Fix exception, when view has no filename (is not saved), fix #27

--- ExpandRegion.py
+++ ExpandRegion.py
@@ -8,14 +8,17 @@ except:
 class ExpandRegionCommand(sublime_plugin.TextCommand):
   def run(self, edit, debug=False):
     
-    name, extension = os.path.splitext(self.view.file_name())
+    extension = ""
+    if (self.view.file_name()):
+      name, fileex = os.path.splitext(self.view.file_name())
+      extension = fileex[1:]
 
     region = self.view.sel()[0]
     string = self.view.substr(sublime.Region(0, self.view.size()))
     start = region.begin()
     end = region.end()
 
-    result = expand_region_handler.expand(string, start, end, extension[1:])
+    result = expand_region_handler.expand(string, start, end, extension)
     if result:
       self.view.sel().add(sublime.Region(result["start"], result["end"]))
       if debug:
